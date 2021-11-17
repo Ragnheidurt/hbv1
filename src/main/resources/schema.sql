c0618a083dc60379a37d24ffb4e3b1edc222c9fb
Guðrún <gha18@hi.is>
2021-11-17 14:35:47 +0000

    prufa sql uppsetningu

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS RECIPE;
+CREATE TABLE RECIPE (
+id INT PRIMARY KEY,
+name VARCHAR(255) UNIQUE,
+description VARCHAR(255),
+instructions VARCHAR(4000),
+serves INT,
+price INT,
+category INT,
+tag INT
+);
+
+DROP TABLE IF EXISTS TAGS;
+CREATE TABLE TAGS (
+id INT PRIMARY KEY,
+vegan BOOLEAN, vegetarian BOOLEAN, 
+vegetarian BOOLEAN,
+glutenfree BOOLEAN,
+dairyfree BOOLEAN,
+kosher BOOLEAN,
+keto BOOLEAN,
+sugarfree BOOLEAN
+);
