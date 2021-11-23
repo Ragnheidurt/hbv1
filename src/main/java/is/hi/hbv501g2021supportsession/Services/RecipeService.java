6d033af92a62dcb701d4412c0b32e5080f39b668
Guðrún <gha18@hi.is>
2021-11-23 16:33:14 +0000

    basic aðferðir

--- src/main/java/is/hi/hbv501g2021supportsession/Services/RecipeService.java
+++ src/main/java/is/hi/hbv501g2021supportsession/Services/RecipeService.java
@@ -8,5 +8,14 @@ public interface RecipeService {
     List<Recipe> findAll();
     Recipe findOne(long id);
     List<Recipe> findByName(String name);
+    List<Recipe> findByCategory(int category);
+    List<Recipe> findByServes(int servesLow, int servesHigh);
+    List<Recipe> findByPrice(int priceLow, int priceHigh);
+    List<Recipe> findByVegan(boolean vegan);
+    List<Recipe> findByVegetarian(boolean vegetarian);
+    List<Recipe> findByGlutenfree(boolean glutenfree);
+    List<Recipe> findByDairyfree(boolean dairyfree);
+    List<Recipe> findByKosher(boolean kosher);
+    List<Recipe> findBySugarfree(boolean sugarfree);
     //void sendRecipe(long id, String email);*/
 }
