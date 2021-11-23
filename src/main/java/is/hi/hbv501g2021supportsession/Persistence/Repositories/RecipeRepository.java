dc0a1454c7c51b7dd233eeec73ebcff3068a0e75
Guðrún <gha18@hi.is>
2021-11-23 16:12:42 +0000

    query?

--- src/main/java/is/hi/hbv501g2021supportsession/Persistence/Repositories/RecipeRepository.java
+++ src/main/java/is/hi/hbv501g2021supportsession/Persistence/Repositories/RecipeRepository.java
@@ -4,14 +4,13 @@ import org.springframework.data.jpa.repository.Query;
 import is.hi.hbv501g2021supportsession.Persistence.Entities.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-//import is.hi.hbv501g2021supportsession.Persistence.Entities.Tags;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findAll();
 
-    @Query(value = "Select * FROM recipe p WHERE p.id = 2", nativeQuery = true)
+    @Query(value = "Select * FROM recipe p WHERE p.id = ?1", nativeQuery = true)
     Recipe findOne(long id);
 
     //@Query(value = "SELECT p FROM recipe p WHERE p.name = 'test'")
